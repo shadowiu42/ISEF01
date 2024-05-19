367885bddce4bdd2d1edaad64ed36f156a49ffe1
shadowiu42 <kerem.atar@iu-study.org>
2024-05-19 12:11:18 +0200

    Forum and Stapel updatet

--- scripts.js
+++ scripts.js
@@ -1,40 +1,25 @@
 $(document).ready(function () {
-    // Funktion zum Laden der gespeicherten Fragen und Antworten
-    function loadQuestions() {
-        var storedQuestions = JSON.parse(localStorage.getItem('questions')) || [];
-        storedQuestions.forEach(function (question) {
-            var newQuestionCard = `
-                <div class="card mb-4">
-                    <div class="card-body">
-                        <h5>User fragt: ${question.text}</h5>
-                        ${question.answers.map(answer => `<p>${answer}</p>`).join('')}
-                        <input type="text" class="form-control mb-2" placeholder="Schreibe hier deine Antwort">
-                        <button class="btn btn-primary btn-antworten">Antworten</button>
+    $('#addCardForm').submit(function (event) {
+        event.preventDefault();
+        var cardTitle = $('#cardTitle').val();
+        if (cardTitle) {
+            var newCard = `
+                <div class="col-md-4 mb-4">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <h5 class="card-title">${cardTitle}</h5>
+                            <button class="btn btn-edit">Bearbeiten</button>
+                            <button class="btn btn-learn">Lernen</button>
+                        </div>
                     </div>
                 </div>
             `;
-            $('#questions-container').prepend(newQuestionCard);
-        });
-    }
+            $('#addCardModal').modal('hide');
+            $('#card-container .add-card-container').before(newCard);
+            $('#cardTitle').val('');
+        }
+    });
 
-    // Funktion zum Speichern der Fragen und Antworten im Local Storage
-    function saveQuestions() {
-        var questions = [];
-        $('#questions-container .card').each(function () {
-            var questionText = $(this).find('h5').text().replace('User fragt: ', '');
-            var answers = [];
-            $(this).find('p').each(function () {
-                answers.push($(this).text());
-            });
-            questions.push({ text: questionText, answers: answers });
-        });
-        localStorage.setItem('questions', JSON.stringify(questions));
-    }
-
-    // Fragen beim Laden der Seite laden
-    loadQuestions();
-
-    // Neue Frage hinzufügen und speichern
     $('#addQuestionForm').submit(function (event) {
         event.preventDefault();
         var newQuestion = $('#newQuestion').val();
@@ -50,11 +35,10 @@ $(document).ready(function () {
             `;
             $('#newQuestion').val('');
             $('#questions-container').prepend(newQuestionCard);
-            saveQuestions(); // Speichern nach Hinzufügen einer neuen Frage
         }
     });
 
-    // Antworten hinzufügen und speichern
+    // Event delegation to handle dynamic answer buttons
     $(document).on('click', '.btn-antworten', function () {
         var answerInput = $(this).siblings('input');
         var answerText = answerInput.val();
@@ -62,7 +46,6 @@ $(document).ready(function () {
             var newAnswer = `<p>${answerText}</p>`;
             $(newAnswer).insertBefore(answerInput);
             answerInput.val('');
-            saveQuestions(); // Speichern nach Hinzufügen einer Antwort
         }
     });
 });
